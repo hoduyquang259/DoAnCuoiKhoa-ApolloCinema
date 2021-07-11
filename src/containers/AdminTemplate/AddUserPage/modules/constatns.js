aaedf299e6e18769702b3e69746bff7427e7c4cc
hoduyquang259 <78310230+hoduyquang259@users.noreply.github.com>
2021-07-11 18:36:44 +0700

    create new code

--- src/containers/AdminTemplate/AddUserPage/modules/constatns.js
+++
@@ -1,3 +0,0 @@
-export const ADD_USER_REQUEST = "@addUserReducer/ADD_USER_REQUEST";
-export const ADD_USER_SUCCESS = "@addUserReducer/ADD_USER_SUCCESS";
-export const ADD_USER_FAILED = "@addUserReducer/ADD_USER_FAILED";
